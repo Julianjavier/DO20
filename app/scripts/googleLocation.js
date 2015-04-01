ec4ad0b1b840906dfab4293be36df70df91ab56e
Julian Rodriguez <greyfox893@mail.com>
2015-04-01 13:33:22 -0400

    built get parameters and tested

--- app/scripts/googleLocation.js
+++ app/scripts/googleLocation.js
@@ -1,6 +1,15 @@
 do20.controller('GoogleLocation', ['$scope', '$http', function($scope, $http){
 	// var goog = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=28.5939260,-81.2941990&radius=1500&types=restaurant&key=AIzaSyA8ovrfj9jOPliuv-tBX9Z9pZgO9s2Cnq4';
-	$http.get('../scripts/getLocation.php')
-        .success(function(data){ console.log('YAYA ',data); })
-        .error(function(data){ console.log('NONO ',data); });
+	var category = 'restaurant'
+	$http.get('../scripts/getLocation.php?category='+category)
+        .success(function(data){
+        	var limit = data.results.length;
+        	var number = Math.floor((Math.random() * 10) + 1);
+        	// console.log('We got ', data);
+        	console.log('We got ', number);
+        	console.log('Witch is', data.results[number]);
+        })
+        .error(function(data){ 
+        	console.log('NONO ',data); 
+        });
 }]);

d9661bd4f51a27ea3e205b9f513db90c9882b88b
Julian Rodriguez <greyfox893@mail.com>
2015-05-14 01:26:54 -0400

    Testing new mongo connection2

--- app/scripts/getUserData.php
+++ app/scripts/getUserData.php
@@ -9,29 +9,30 @@ $key = true;
 
 if ($key == true) {
 	$mongo = new MongoClient("mongodb://${Bodark216}:${Durandal216}@localhost");
-	$cursor = $mongo ->selectDb('do20')->selectCollection('userData');
+	echo "MONGO CLIENT";
+	// $cursor = $mongo ->selectDb('do20')->selectCollection('userData');
 
-	$filter = array('firstName' => $firstName);
+	// $filter = array('firstName' => $firstName);
 
-	$data = $cursor->find( $filter );
-	$length = count(iterator_to_array($data));
+	// $data = $cursor->find( $filter );
+	// $length = count(iterator_to_array($data));
 	
-	if ($length <= 0) {
-		$data = $cursor->insert(
-		array(
-			"firstName" => $firstName,
-			"lastName" => $lastName,
-			"id" => $id,
-			"score"=> intval(0),
-			"tasks" => intval(0)
-			)
-		);	
+	// if ($length <= 0) {
+	// 	$data = $cursor->insert(
+	// 	array(
+	// 		"firstName" => $firstName,
+	// 		"lastName" => $lastName,
+	// 		"id" => $id,
+	// 		"score"=> intval(0),
+	// 		"tasks" => intval(0)
+	// 		)
+	// 	);	
 
-		$data = $cursor->find($filter);
-		$id = $data->_id;
-		echo "[".json_encode(iterator_to_array($data))."]";	
+	// 	$data = $cursor->find($filter);
+	// 	$id = $data->_id;
+	// 	echo "[".json_encode(iterator_to_array($data))."]";	
 	}else{
-		echo "[".json_encode(iterator_to_array($data))."]";
+		// echo "[".json_encode(iterator_to_array($data))."]";
 	};
 
 }

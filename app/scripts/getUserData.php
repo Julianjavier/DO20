4967d29e1fc4776311ef8e1c825eb55a536e99d4
Julian Rodriguez <greyfox893@mail.com>
2015-05-14 01:37:54 -0400

    Testing new mongo connection. Removed if statment and left simple echo.

--- app/scripts/getUserData.php
+++ app/scripts/getUserData.php
@@ -7,34 +7,34 @@ $id = ($_GET['id']);
 
 $key = true;
 echo "THIS IS BEFORE THE IF";
-if ($key == true) {
-	// $mongo = new MongoClient("mongodb://${Bodark216}:${Durandal216}@localhost");
-	echo "MONGO CLIENT";
-	// $cursor = $mongo ->selectDb('do20')->selectCollection('userData');
+// if ($key == true) {
+// 	// $mongo = new MongoClient("mongodb://${Bodark216}:${Durandal216}@localhost");
+// 	echo "MONGO CLIENT";
+// 	// $cursor = $mongo ->selectDb('do20')->selectCollection('userData');
 
-	// $filter = array('firstName' => $firstName);
+// 	// $filter = array('firstName' => $firstName);
 
-	// $data = $cursor->find( $filter );
-	// $length = count(iterator_to_array($data));
+// 	// $data = $cursor->find( $filter );
+// 	// $length = count(iterator_to_array($data));
 	
-	// if ($length <= 0) {
-	// 	$data = $cursor->insert(
-	// 	array(
-	// 		"firstName" => $firstName,
-	// 		"lastName" => $lastName,
-	// 		"id" => $id,
-	// 		"score"=> intval(0),
-	// 		"tasks" => intval(0)
-	// 		)
-	// 	);	
+// 	// if ($length <= 0) {
+// 	// 	$data = $cursor->insert(
+// 	// 	array(
+// 	// 		"firstName" => $firstName,
+// 	// 		"lastName" => $lastName,
+// 	// 		"id" => $id,
+// 	// 		"score"=> intval(0),
+// 	// 		"tasks" => intval(0)
+// 	// 		)
+// 	// 	);	
 
-	// 	$data = $cursor->find($filter);
-	// 	$id = $data->_id;
-	// 	echo "[".json_encode(iterator_to_array($data))."]";	
-	}else{
-		// echo "[".json_encode(iterator_to_array($data))."]";
-	};
+// 	// 	$data = $cursor->find($filter);
+// 	// 	$id = $data->_id;
+// 	// 	echo "[".json_encode(iterator_to_array($data))."]";	
+// 	}else{
+// 		// echo "[".json_encode(iterator_to_array($data))."]";
+// 	};
 
-}
+// }
 
 ?>

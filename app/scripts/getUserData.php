c350efa509744acdd8f7a45bac3ec7ead51bf896
Julian Rodriguez <greyfox893@mail.com>
2015-05-15 15:30:05 -0400

    Get user data scripts ave been fixed and finished.

--- app/scripts/getUserData.php
+++ app/scripts/getUserData.php
@@ -6,37 +6,43 @@ $id = ($_GET['id']);
 
 
 $key = true;
+$output = array();
 
 echo "Before connection ";
 if ($key == true) {
-	echo " after IF";
-	$mongo = new MongoClient("mongodb://julianjavier:drebin893@localhost");
-	echo "after connection ". $mongo;
+	echo "after IF ";
+	$mongo = new MongoClient("mongodb://julianjavier:drebin893@localhost/do20");
+	echo "after connection ";
 
-	// $cursor = $mongo ->selectDb('do20')->selectCollection('userData');
+	$cursor = $mongo->selectDb('do20')->selectCollection('userData');
+	$filter = array('firstName' => $firstName);
 
-	// $filter = array('firstName' => $firstName);
-
-	// $data = $cursor->find( $filter );
-	// $length = count(iterator_to_array($data));
+	$data = $cursor->find($filter);
 	
-	// if ($length <= 0) {
-	// 	$data = $cursor->insert(
-	// 	array(
-	// 		"firstName" => $firstName,
-	// 		"lastName" => $lastName,
-	// 		"id" => $id,
-	// 		"score"=> intval(0),
-	// 		"tasks" => intval(0)
-	// 		)
-	// 	);	
+	$length = count(iterator_to_array($data));
+	
+	if ($length <= 0) {
+		$data = $cursor->insert(
+		array(
+			"firstName" => $firstName,
+			"lastName" => $lastName,
+			"id" => $id,
+			"score"=> intval(0),
+			"tasks" => intval(0),
+			"mileList" => array()
+			)
+		);	
 
-	// 	$data = $cursor->find($filter);
-	// 	$id = $data->_id;
-	// 	echo "[".json_encode(iterator_to_array($data))."]";	
-	// }else{
-	// 	echo "[".json_encode(iterator_to_array($data))."]";
-	// };
+		echo "[".json_encode(iterator_to_array($output))."]";	
+	}else{
+        foreach($data as $result){
+            $result['firstName'];
+            $result['score'];
+            $result['tasks'];
+            $obj = array('firstName' => $result['firstName'] , 'score' => $result['score'], 'tasks' => $result['tasks']);
+            echo json_encode($obj);
+        };
+	};
 
 }
 
